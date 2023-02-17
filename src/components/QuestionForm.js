2b5b65db1e1b780d84f4f76a86a4072b34e61c58
Eddington Rowellis <eddingtonibi@gmail.com>
2023-02-17 14:02:08 +0300

    completed assignment

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -1,25 +1,130 @@
+// import React, { useState } from "react";
+
+// function QuestionForm(props) {
+//   const [formData, setFormData] = useState({
+//     prompt: "",
+//     answer1: "",
+//     answer2: "",
+//     answer3: "",
+//     answer4: "",
+//     correctIndex: 0,
+//   });
+
+//   function handleChange(event) {
+//     setFormData({
+//       ...formData,
+//       [event.target.name]: event.target.value,
+//     });
+//   }
+
+//   function handleSubmit(event) {
+//     event.preventDefault();
+//     console.log(formData);
+//   }
+
+//   return (
+//     <section>
+//       <h1>New Question</h1>
+//       <form onSubmit={handleSubmit}>
+//         <label>
+//           Prompt:
+//           <input
+//             type="text"
+//             name="prompt"
+//             value={formData.prompt}
+//             onChange={handleChange}
+//           />
+//         </label>
+//         <label>
+//           Answer 1:
+//           <input
+//             type="text"
+//             name="answer1"
+//             value={formData.answer1}
+//             onChange={handleChange}
+//           />
+//         </label>
+//         <label>
+//           Answer 2:
+//           <input
+//             type="text"
+//             name="answer2"
+//             value={formData.answer2}
+//             onChange={handleChange}
+//           />
+//         </label>
+//         <label>
+//           Answer 3:
+//           <input
+//             type="text"
+//             name="answer3"
+//             value={formData.answer3}
+//             onChange={handleChange}
+//           />
+//         </label>
+//         <label>
+//           Answer 4:
+//           <input
+//             type="text"
+//             name="answer4"
+//             value={formData.answer4}
+//             onChange={handleChange}
+//           />
+//         </label>
+//         <label>
+//           Correct Answer:
+//           <select
+//             name="correctIndex"
+//             value={formData.correctIndex}
+//             onChange={handleChange}
+//           >
+//             <option value="0">{formData.answer1}</option>
+//             <option value="1">{formData.answer2}</option>
+//             <option value="2">{formData.answer3}</option>
+//             <option value="3">{formData.answer4}</option>
+//           </select>
+//         </label>
+//         <button type="submit">Add Question</button>
+//       </form>
+//     </section>
+//   );
+// }
+
+// export default QuestionForm;
+
+
 import React, { useState } from "react";
 
-function QuestionForm(props) {
+function QuestionForm({ onAddQuestion }) {
   const [formData, setFormData] = useState({
     prompt: "",
-    answer1: "",
-    answer2: "",
-    answer3: "",
-    answer4: "",
+    answers: ["", "", "", ""],
     correctIndex: 0,
   });
 
   function handleChange(event) {
-    setFormData({
+    const newFormData = {
       ...formData,
-      [event.target.name]: event.target.value,
-    });
+      [event.target.name]:
+        event.target.name === "answers"
+          ? [
+              ...formData.answers.slice(0, Number(event.target.id)),
+              event.target.value,
+              ...formData.answers.slice(Number(event.target.id) + 1),
+            ]
+          : event.target.value,
+    };
+    setFormData(newFormData);
   }
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(formData);
+    const newQuestion = {
+      prompt: formData.prompt,
+      answers: formData.answers,
+      correctIndex: Number(formData.correctIndex),
+    };
+    onAddQuestion(newQuestion);
   }
 
   return (
@@ -39,8 +144,9 @@ function QuestionForm(props) {
           Answer 1:
           <input
             type="text"
-            name="answer1"
-            value={formData.answer1}
+            name="answers"
+            id="0"
+            value={formData.answers[0]}
             onChange={handleChange}
           />
         </label>
@@ -91,3 +197,4 @@ function QuestionForm(props) {
 }
 
 export default QuestionForm;
+
